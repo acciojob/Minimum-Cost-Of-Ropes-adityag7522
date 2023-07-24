8d28b94942e7ce6837cb7e65ca0aa73937bc5f06
adityag7522 <66068085+adityag7522@users.noreply.github.com>
2023-07-24 22:26:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+//your code here
+let lengths = document.getElementById('rope-lengths');
+let values = lengths.value.split(', ').map(Number);
+values.sort((a, b) => a - b);
+let ans = 0;
+while(values.length>2){
+let val1 = values[0];
+let val2 = values[1];
+values.splice(0,2);
+ans+=val1+val2;
+values.push(val1+val2);
+values.sort((a, b) => a - b);
+}
+ans+=values[0]+values[1];
+let result = document.getElementById('result');
+result.textContent = ans;
+}
